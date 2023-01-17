88e3ae0c047a02ef5f4754b87217da9ec8bc6471
Ranbut <Ranbut_Beater@protonmail.com>
2023-01-17 18:18:09 -0300

    Configurações configurado MongoDB

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import joi from 'joi';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +10,26 @@ server.use(express.json());
 server.use(cors());
 
 const PORT = 5000;
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+
+let db;
+
+try{
+    await mongoClient.connect()
+    db = mongoClient.db();
+    console.log("Banco de dados MongoDB conectado!");
+}catch(err){
+    console.log(`Erro ao conectar ao MongoDB: ${err}`);
+};
+
+server.post("/sign-up", async (req, res)=>{
+
+});
+
+server.get("/login", async (req, res)=>{
+
+});
 
 server.listen(PORT, () => {
   console.log(`Servidor iniciado na porta: ${PORT}`);
